cd5af5d610e874f67cf9afb513c552b403fb40f2
ta <tkey.dll@gmail.com>
2025-08-14 17:48:03 +0900

    全てのAPI関連ファイルを削除し、プロジェクトをクリーンアップ

--- functions/api/ping/index.js
+++
@@ -1,3 +0,0 @@
-export async function run (context, req) {
-  context.res = { status: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ ok: true, time: new Date().toISOString() }) };
-}
